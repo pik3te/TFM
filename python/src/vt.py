4f55e15fe3e345ac839e55d439e001e81b22b790
SotohFTW <sotohftw@gmail.com>
2022-01-24 12:08:37 -0500

    Triying to loop through the dataset directory and making request to VT API

--- python/src/vt.py
+++ python/src/vt.py
@@ -1,26 +1,51 @@
 import requests
 import hashlib
+import os
 
 apikey = open("../../vtoken", "r").read().rstrip('\n')
-file_payload = open("../../dataset/Objective_See/Ventir/Ventir", "rb").read()
-
-md5Hash = hashlib.md5(file_payload)
-md5Hashed = md5Hash.hexdigest()
-
-print(md5Hashed)
-
-url = "https://www.virustotal.com/api/v3/files/"+md5Hashed
-
-print(url)
-
-payload = file_payload
-
-headers = {
-    "Accept": "application/json",
-    "x-apikey": apikey,
-}
-
-response = requests.request("GET", url, headers=headers)
 
 
-print(response.text)
+
+for filename in os.listdir("files"):
+    open(os.path.join("files", filename),'rb') as f:
+        text = f.read()
+        md5Hash = hashlib.md5(f)
+        md5Hashed = md5Hash.hexdigest()
+        print(vtrequest(md5Hashed))
+
+def vtrequest(md5hashed):
+    headers = {
+        "Accept":"application/json",
+        "x-apikey":apikey,
+    }
+    url = "https://www.virustotal.com/api/v3/files/"+md5hashed
+    response = request.resquest("GET", url, headers = headers)
+    return response
+
+
+
+
+
+# 
+# file_payload = open("../../dataset/Objective_See/Ventir/Ventir", "rb").read()
+# 
+# md5Hash = hashlib.md5(file_payload)
+# md5Hashed = md5Hash.hexdigest()
+# 
+# print(md5Hashed)
+# 
+# url = "https://www.virustotal.com/api/v3/files/"+md5Hashed
+# 
+# print(url)
+# 
+# payload = file_payload
+# 
+# headers = {
+#     "Accept": "application/json",
+#     "x-apikey": apikey,
+# }
+# 
+# response = requests.request("GET", url, headers=headers)
+# 
+# json = open("../../json", "w").write(response.text)
+# print(response.text)
