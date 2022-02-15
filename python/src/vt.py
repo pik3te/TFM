b2981afbce410ae5196fe703d69893682e18d552
SotohFTW <sotohftw@gmail.com>
2022-02-15 12:00:14 -0500

    changed md5 to sha1 hashes, stored all json into one folder for post analysis

--- python/src/vt.py
+++ python/src/vt.py
@@ -6,37 +6,38 @@ import datetime
 
 apikey = open("../../vtoken", "r").read().rstrip('\n')
 
-def vtrequest(md5hashed):
+def vtrequest(sha1hashed):
     headers = {
         "Accept":"application/json",
         "x-apikey":apikey,
     }
-    url = "https://www.virustotal.com/api/v3/files/"+md5hashed
+    url = "https://www.virustotal.com/api/v3/files/"+sha1hashed
     response = requests.request("GET", url, headers = headers)
     return response
 
 for filename in os.listdir("../../dataset"):
     f=open(os.path.join("../../dataset", filename),'rb')
     text = f.read()
-    md5Hash = hashlib.md5(text)
-    md5Hashed = md5Hash.hexdigest()
-    jsondecoded = json.loads(vtrequest(md5Hashed).text)
-    print(jsondecoded['data']['attributes']['names'][0], end = ', ') 
+    sha1Hash = hashlib.sha1(text)
+    sha1Hashed = sha1Hash.hexdigest()
+    open("../../JSON/"+filename+".json","w").write(vtrequest(sha1Hashed).text)
+#    jsondecoded = json.loads(vtrequest(sha1Hashed).text)
+#    print(jsondecoded['data']['attributes']['names'][0], end = ', ') 
 #    print(jsondecoded['data']['attributes']['trid'][1]['file_type'], end = ', ')
-    print(jsondecoded['data']['attributes']['reputation'], end = ', ')
-    print(datetime.datetime.fromtimestamp(jsondecoded['data']['attributes']['first_submission_date']), end= ', ')
-    print(datetime.datetime.fromtimestamp(jsondecoded['data']['attributes']['last_submission_date']), end= ', ')
-    print('\n')
+#    print(jsondecoded['data']['attributes']['reputation'], end = ', ')
+#    print(datetime.datetime.fromtimestamp(jsondecoded['data']['attributes']['first_submission_date']), end= ', ')
+#    print(datetime.datetime.fromtimestamp(jsondecoded['data']['attributes']['last_submission_date']), end= ', ')
+#    print('\n')
 
 # 
 # file_payload = open("../../dataset/Objective_See/Ventir/Ventir", "rb").read()
 # 
-# md5Hash = hashlib.md5(file_payload)
-# md5Hashed = md5Hash.hexdigest()
+# sha1Hash = hashlib.sha1(file_payload)
+# sha1Hashed = sha1Hash.hexdigest()
 # 
-# print(md5Hashed)
+# print(sha1Hashed)
 # 
-# url = "https://www.virustotal.com/api/v3/files/"+md5Hashed
+# url = "https://www.virustotal.com/api/v3/files/"+sha1Hashed
 # 
 # print(url)
 # 
