19d1549e88a4935f7e313130eb8cd05664a27416
SotohFTW <sotohftw@gmail.com>
2022-02-22 10:32:58 -0500

    añadidos campos para posterior análisis

--- python/src/jtc.py
+++ python/src/jtc.py
@@ -7,13 +7,14 @@ for filename in os.listdir("../../json"):
     f=open(os.path.join("../../json", filename),'r')
     text = f.read()
     jsondecoded = json.loads(text)
-    print(filename)
+    print(jsondecoded['data']['attributes']['sha1'], end = ', ') 
     print(jsondecoded['data']['attributes']['type_description'], end = ', ') 
     print(jsondecoded['data']['attributes']['size'], end = ', ') 
-    print(datetime.datetime.fromtimestamp(jsondecoded['data']['attributes']['first_submission_date']), end= ', ')
-    print(jsondecoded['data']['attributes']['sha1'], end = ', ') 
-    print(jsondecoded['data']['attributes']['popular_threat_classification'], end = ', ') 
+    print(jsondecoded['data']['attributes']['popular_threat_classification']['suggested_threat_label'], end = ', ') 
+    for engine in jsondecoded['data']['attributes']['last_analysis_results']:
+        print(jsondecoded['data']['attributes']['last_analysis_results'][engine]['category'], end= ', ')
     print(datetime.datetime.fromtimestamp(jsondecoded['data']['attributes']['last_submission_date']), end= ', ')
+    print(datetime.datetime.fromtimestamp(jsondecoded['data']['attributes']['first_submission_date']), end= ', ')
     print('\n')
 
 #    print(jsondecoded['data']['attributes']['trid'][1]['file_type'], end = ', ')
