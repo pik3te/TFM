0cd4de7d569aba68abfe2735df2278506211f26c
SotohFTW <sotohftw@gmail.com>
2022-02-24 17:13:02 -0500

    trying different solutions to the engines problem

--- python/src/jtc.py
+++ python/src/jtc.py
@@ -1,38 +1,31 @@
 import os
 import json
 import datetime
+import numpy as np
+import pandas as pd
 
-c=open("../../demo.csv","w")
-t=open("../../json/0.json")
+output = open("../../demo.json","w")
 
-title_text = t.read()
-json_title_decoded = json.loads(title_text)
-c.write("sha1"+', ')
-c.write("type"+', ')
-c.write("category"+', ')
-for engine in json_title_decoded['data']['attributes']['last_analysis_results']:
-    c.write(engine+', ')
-c.write ("last_time"+', ')
-c.write ("firs_time")
-c.write('\n')
+result = []
 
 for filename in os.listdir("../../json"):
-    f=open(os.path.join("../../json", filename),'r')
+    f = open(os.path.join("../../json", filename),'r')
     text = f.read()
-    jsondecoded = json.loads(text)
-    c.write(jsondecoded['data']['attributes']['sha1']+', ') 
-    c.write(jsondecoded['data']['attributes']['type_description']+', ') 
-    c.write(str(jsondecoded['data']['attributes']['size'])+', ') 
-    c.write(jsondecoded['data']['attributes']['popular_threat_classification']['popular_threat_category'][0]['value']+', ') 
-    c.write(str(datetime.datetime.fromtimestamp(jsondecoded['data']['attributes']['last_submission_date']))+', ')
-    c.write(str(datetime.datetime.fromtimestamp(jsondecoded['data']['attributes']['first_submission_date']))+', ')
-    for engine in jsondecoded['data']['attributes']['last_analysis_results']:
-        c.write(jsondecoded['data']['attributes']['last_analysis_results'][engine]['category']+', ')
-    c.write('\n')
-#    c.write(jsondecoded['data']['attributes']['trid'][1]['file_type']+', ')
-#    c.write(jsondecoded['data']['attributes']['reputation']+', ')
-#    
-#    
+    json_decode = json.loads(text)
+    my_dict={}
+    my_dict['sha1'] = json_decode.get('data').get('attributes').get('sha1')
+    my_dict['type'] = json_decode.get('data').get('attributes').get('type_description')
+    my_dict['size'] = json_decode.get('data').get('attributes').get('size')
+    my_dict['first'] = json_decode.get('data').get('attributes').get('first_submission_date')
+    my_dict['last'] = json_decode.get('data').get('attributes').get('last_submission_date')
+    result.append(my_dict)
+    
 
-c=open("../../demo.csv","r")
-print(c.read())
+back_json = json.dumps(result)
+output.write(back_json)
+print(back_json)
+df = pd.read_json(back_json)
+print(df.head())
+print(df.info())
+print(df.tail())
+
